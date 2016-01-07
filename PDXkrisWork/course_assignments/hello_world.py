904a16d3d28edeba33a11d7db401a3abba59fc65
kriskuchinka <kriskuchinka@gmail.com>
2016-01-07 14:03:48 -0800

    Enhanced Hello World.

--- PDXkrisWork/course_assignments/hello_world.py
+++ PDXkrisWork/course_assignments/hello_world.py
@@ -5,4 +5,7 @@ age = input("How old are you? ")
 
 age_saying_1 = ". You are"
 age_saying_2 = "years old."
-print(phrase, name,age_saying_1, age, age_saying_2)
+
+output = phrase + " " + name + " " + age_saying_1 + " " + age + " " + age_saying_2
+
+print(output)
