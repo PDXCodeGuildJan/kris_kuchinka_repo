8fe71a97a62fa5f65818a404842d9ae6594d8682
kriskuchinka <kriskuchinka@gmail.com>
2016-01-11 09:38:48 -0800

    More progress on the game.

--- PDXkrisWork/course_assignments/maze.py
+++ PDXkrisWork/course_assignments/maze.py
@@ -1,4 +1,5 @@
 # Date Started: 2015.01.08
+# Date Finished: 
 # Created By: Kris Kuchinka
 # Assignment at PDX Code Guild
 #----------------------------------->
@@ -31,13 +32,13 @@ def main():
 	print (" ")
 	print("You never, ever thought it would happen to you, but it did-- you have been abducted and are now waking up in an unknown place. Welcome to Dark Moor Pond.")
 	print (" ") 
-	print ("There is a way out, but you have to find it on your own. I wanted to play a game. Now you are the player. Your goal is to make it to the parking lot. IF you decide to simply give up and suicide, hit 'q' and the game will be over.")
+	print ("There is a way out, but you have to find it on your own. This is a cruel game. Now you are the player. Your goal is to make it to the parking lot. IF you decide to simply give up, hit 'q' and the game will be over. But that will mean you lose.")
 	print (" ")
-	print ("But that's not like you, is it? You want to find your way to the parking lot and you know you can.")
+	print ("That's not like you, is it? You want to find your way to the parking lot. You CAN do it. You just have to get througha small maze first.")
 	print (" ")
-	print ("It's as simple as that. Then there is an Audi waiting for you, the key is in a magnetic case under the passenger side tire well. You can make it out alive. Until then, enjoy...")
+	print ("It's as simple as that. Then there is an Audi waiting for you, the key is in a magnetic case under the passenger side tire well. You can make it out alive. You even get to keep the car as a reward. Enjoy the game.")
 	print (" ")
-
+	# User starts out in the Lodge
 	lodge()
 
 
@@ -50,9 +51,9 @@ def lodge():
 	print ("The point now is to find the parking lot.")
 	print (" ")
 	print ("In the lodge, you have 3 options to choose from.")
-	print ("Option 1 is to head to the door in the forward, left diagnol corner of the Lodge.")
-	print ("Option 2 is to head to the door in the forward, right diagnol corner of the Lodge.")
-	print ("Option 3 is to head straight forward to the door on the opposite end of the Lodge.")
+	print ("Option 1 is to head to the door in the North-East corner of the Lodge.")
+	print ("Option 2 is to head to the door in the South-East corner of the Lodge.")
+	print ("Option 3 is to head East to the door on the opposite end of the Lodge.")
 	print ("Or you can simply type 'q' and give up, but that wouldn't be any fun, now would it?")
 	print (" ")
 	# set flag to false for correct_answer variable
@@ -65,26 +66,36 @@ def lodge():
 		if choice == "1":
 			correct_answer = True
 			print (" ")
+			print ("You are walking down a long hallway. I wonder where this heads.")
+			print (" ")
 			kitchen()
 		# Choice 2 leads to the Library
 		elif choice == "2":
 			correct_answer = True
+			print (" ")
+			print ("This hallway is well lit. You can see the door to the next room at the end. Go ahead and walk through.")
 			print (" ")
 			library()
 		# Choice 3 leads to the Viewing Deck
 		elif choice == "3":
 			correct_answer = True
 			print (" ")
+			print ("Do you feel that draft? It's starting to get cold.")
+			print (" ")
 			viewing_deck()
 		# The ultimate out--> they quit:-(
 		elif choice == "q":
 			correct_answer = True
 			print (" ")
+			print ("You gave up? I can't believe it. It looks like you lost the game.")
+			print (" ")
 			exit()
-		# repeat the options
+		# Repeat the options
 		else:
 			correct_answer = False
+			print (" ")
 			print ("You didn't choose one of the 3 options. You didn't even choose to quit. Now you have to start over at the Lodge.")
+			print (" ")
 			lodge()
 
 
@@ -93,11 +104,12 @@ def kitchen():
 	print (" ")
 	print ("Welcome to the Kitchen.")
 	print (" ")
-	print ("I'd like to say that many have shared meals in here, but others have been part of the share, if you know what I'm saying.")
+	print ("Many have shared meals in here, but others have been part of the share, if you know what I'm saying.")
 	print (" ")
 	print ("In the kitchen you have two options to choose from.")
-	print ("Option 1 is to walk through the door to the rear diagnol corner.")
-	print ("Option 2 is to walk to the door in the forward right of the room.")
+	print ("Option 1 is to walk through the South-West door.")
+	print ("Option 2 is to walk through the South door.")
+	print (" ")
 
 	# set flag to false for correct_answer variable
 	correct_answer = False
@@ -110,6 +122,7 @@ def kitchen():
 			correct_answer = True
 			print (" ")
 			print ("It looks like you are headed to a familiar room.")
+			print (" ")
 			lodge()
 		# Choice 2 leads to the Library
 		elif choice == "2":
@@ -120,6 +133,9 @@ def kitchen():
 			library()
 		elif choice == "q":
 			correct_answer = True
+			print (" ")
+			print ("You gave up? I can't believe it. It looks like you lost the game.")
+			print (" ")
 			exit()
 		else: 
 			correct_answer = False
@@ -132,14 +148,13 @@ def library():
 	print (" ")
 	print ("Welcome to the Library.")
 	print (" ")
-	print ("This room is full of so many books, all with so much knowledge to give, but, try as you might, you will not find a map of Dark Moor Pond in here.")
+	print ("This room is full of many books, all with so much knowledge to give, but, try as you might, you will not find a map of Dark Moor Pond in here.")
 	print (" ")
 	print ("You have 3 options to choose from.")
-	print ("Option 1 lets you move diagnolly to the left rear.")
-	print ("Option 2 makes you move to the left side of the room and through that door.")
-	print ("Option 3 means you move diagnolly to the left corner and walk through that doorway.")
+	print ("Option 1 is the door in the North-West corner of the Library.")
+	print ("Option 2 is the door on the North wall.")
+	print ("Option 3 is the door in the North-East corner of the room.")
 	print (" ")
-
 
 	# set flag to false for correct_answer variable
 	correct_answer = False
@@ -158,19 +173,22 @@ def library():
 		elif choice == "2":
 			correct_answer = True
 			print (" ")
-			print ("You are now heading back down the corridor that brought you to the kitchen. It's cold in here and you are headed backwards.")
+			print ("I hope you are hungry. It looks like you're headed towards a snack.")
 			print (" ")
 			kitchen()
 		# Choice 3 leads to the Naughty Room
 		elif choice == "3":
 			correct_answer = True
 			print (" ")
-			print ("You walk into a dark hallway. Wherever you're headed, it can't be good. The door at the end of the hallway creaks open when push on it. You have wandered into the Naughty Room.")
+			print ("You walk into a dark hallway. Wherever you're headed, it can't be good. The door at the end of the hallway creaks open when you push on it. You have wandered into the Naughty Room.")
 			print (" ")
 			naughty_room()
 		# Or user can choose to quit
 		elif choice == "q":
 			correct_answer = True
+			print (" ")
+			print ("You gave up? I can't believe it. It looks like you lost the game.")
+			print (" ")
 			exit()
 		else:
 			print (" ")
@@ -183,13 +201,13 @@ def naughty_room():
 	print (" ")
 	print ("Welcome to the Naughty Room.")
 	print (" ")
-	print ("Kids do bad things and sometimes there's no way to stop them. That's why the Naughty Room was created in the early 1800's. The room is soundproof, so don't bother screaming. At least you aren't locked in. You don't have to stay here, if you choose not to do so.")
+	print ("Kids do bad things and sometimes there's no way to stop them. That's why the Naughty Room was created in the early 1800's. The room is soundproof, so don't bother screaming. At least you aren't locked in. You don't have to stay here. The children always did.")
 	print (" ")
 	print ("You have 4 options to choose from.")
-	print ("Option 1 is the door in the rear right diagnol corner of the room.")
-	print ("Option 2 is the door directly in the rear of the room.")
-	print ("Option 3 leads to door in the forward left diagnol corner.")
-	print ("Option 4 is the door in the forward right diagnol corner.")
+	print ("Option 1 is the door in the South-West corner of the room.")
+	print ("Option 2 is the door on the West wall.")
+	print ("Option 3 is the door in the North-East corner of the room.")
+	print ("Option 4 is the door in the South-East corner of the room.")
 	print (" ")
 
 	# set flag to false for correct_answer variable
@@ -199,13 +217,47 @@ def naughty_room():
 		#list the choices that the user has
 		choice = input("Which of the 4 options do you choose from?    ")
 		# Option 1 is the Library
-		# Option 2 is the Kitchen
+		if choice == "1":
+			correct_answer = True
+			print (" ")
+			print ("I think I smell parchment paper.")
+			print (" ")
+			library()		
+		# Option 2 is the Viewing Deck
+		elif choice == "2":
+			correct_answer = True
+			print (" ")
+			print ("I hope you brought a jacket. Where you're headed, it's cold and foggy.")
+			print (" ")
+			viewing_deck()
 		# Option 3 is the Green House
+		elif choice == "3":
+			correct_answer = True
+			print (" ")
+			print ("You didn't happen to bring a trowel or some potting soil, did you?")
+			print (" ")
+			green_house()
 		# Option 4 is the Parking Lot
+		elif choice == "4":
+			correct_answer = True
+			print (" ")
+			print ("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			print ("I'm getting a good feeling....")
+			print (" ")
+			parking_lot()
 		# Or the user can quit the game
+		elif choice == "q":
+			correct_answer = True
+			print (" ")
+			print ("You gave up? I can't believe it. It looks like you lost the game.")
+			print (" ")
+			exit()
 		# If one of the 5 options is not chosen...
-
-
+		else:
+			print (" ")
+			print ("Please choose one of the choices you've been given. There are 4 options, but you seem to have chosen an option that does not match the choices you have to make.")
+			print(" ")
+			naughty_room()
 
 
 def viewing_deck():
