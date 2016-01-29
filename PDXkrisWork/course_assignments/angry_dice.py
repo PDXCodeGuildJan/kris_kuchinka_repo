e49773c2515a2e080b7de3dc21227d8b12cf5547
kriskuchinka <kriskuchinka@gmail.com>
2016-01-29 12:18:51 -0800

    More additions to program. Especially in regards to ASCII images.

--- PDXkrisWork/course_assignments/angry_dice.py
+++ PDXkrisWork/course_assignments/angry_dice.py
@@ -8,65 +8,111 @@ from random import randint
 
 def main():
 
+	print(sides[1])
+
 class Angry_Dice:
 	"""Defines the variables and methods needed to play the game."""
-#!#!#!#!-- I DON'T KNOW WHAT TO PUT IN THE PARENS BELOW
 	def __init__(self, player_name):
 		# initalize necessary variables for Angry_Dice
-		self.current_stage: 1
-		self.current_goal: []
-		self.player_name: ""
+		self.current_stage = 1
+		self.current_goal = []
+		self.player_name = ""
 		# initialize a dictionary for the Die
 #!#!#!#!# This may be wrong #!#!#!#!#
-		self.die_1: {Die}
-		self.die_2: {Die}
+		self.die_1 = Die()
+		self.die_2 = Die()
 
-	"""Define method to roll dice"""
+	
 	def roll_die(self, die_1, die_2):
+		"""Define method to roll dice"""
 		pass
 	
-	"""Define method to check if both die are 'angry'"""
+	
 	def check_angry(self, die_1, die_2):
+		"""Define method to check if both die are 'angry'"""
 		pass
 	
-	"""Define method that informs player they have won their current round"""
+	
 	def win_round(self, current_stage):
+		"""Define method that informs player they have won their current round"""
 		pass
 	
-	"""Method to allow player to hold 1, roll 1 or roll both die again"""
+	
 	def palyer_choice(self, die_1, die_2):
+		"""Method to allow player to hold 1, roll 1 or roll both die again"""
 		pass
 
-	"""Method to evaluate and ensure that game rules are followed, i.e. invalid dice can not be held"""
+	
 	def evaluate_hold(self, die_1, die_2):
+		"""Method to evaluate and ensure that game rules are followed, i.e. invalid dice can not be held"""
 		pass
 
-	"""Method to advance player to next stage in the game or to the winning stage"""
+		
 	def advance_round(current_stage):
+		"""Method to advance player to next stage in the game or to the winning stage"""
 		pass
 
 
-"""Define object that describes dice and implements their actions (refer to imported random library for rolling authenticity)"""
+
 class Die:
+	"""Define object that describes dice and implements their actions (refer to imported random library for rolling authenticity)"""
+	def __init__(self):
+		"""Define Die library that is comprised of the various dice sides."""
+		self.value = 1
+
+
+	def roll_dice(self):
+		"""Method to roll the dice for game play"""
+		pass
 
 
 
+sides = {
+	1: """    
+	-----  
+       |     |
+       |  0  |
+       |     |
+        -----  
+	   """,
 
+	2: """    -----  
+			 |0    |
+			 |     |
+			 |    0|
+			  -----  
+	   """,
 
+	3: """    -----  
+			 | o o |
+			 |  |  |
+			 | ### |
+			  -----  
+			  RARRR!
+	   """,
 
+	4: """    -----  
+			 |0   0|
+		     |     |
+			 |0   0|
+			  -----  
+	   """,
 
+	5: """    -----  
+			 |0   0|
+			 |  0  |
+			 |0   0|
+			  -----  
+	   """,
 
+	6: """     -----  
+			  |0   0|
+			  |0   0|
+			  |0   0|
+			   -----  
+	   """	
 
-
-
-
-
-
-
-
-
-
-
+}				    					  
 
 
 
