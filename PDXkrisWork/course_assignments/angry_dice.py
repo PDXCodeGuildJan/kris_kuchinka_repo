5693d5c516ef743e04288a85d8d507433f6dbaa8
kriskuchinka <kriskuchinka@gmail.com>
2016-01-29 16:10:39 -0800

    Further addition to the mess.

--- PDXkrisWork/course_assignments/angry_dice.py
+++ PDXkrisWork/course_assignments/angry_dice.py
@@ -7,8 +7,12 @@ from random import randint
 
 
 def main():
+	current_stage = 1
+	angry = Angry_Dice("Jim")
+	angry.roll_dice()
+	angry.check_angry()
+	print(current_stage)
 
-	print(sides[1])
 
 class Angry_Dice:
 	"""Defines the variables and methods needed to play the game."""
@@ -23,32 +27,46 @@ class Angry_Dice:
 		self.die_2 = Die()
 
 	
-	def roll_die(self, die_1, die_2):
-		"""Define method to roll dice"""
-		pass
+	def roll_dice(self):
+		"""Method to roll the dice for game play"""
+		
+		if not self.die_1.locked:
+			# Roll the dice we need to roll
+			self.die_1.roll()
+
+		if not self.die_2.locked:
+			# Roll the dice we need to roll
+			self.die_2.roll()
+
+		print(self.die_1)
+		print(self.die_2)
+
 	
 	
-	def check_angry(self, die_1, die_2):
+	def check_angry(self):
 		"""Define method to check if both die are 'angry'"""
-		pass
+		
+		if self.die_1.value == 3 and self.die_2.value == 3:
+			print("angry!!!")
+			current_stage = 1
 	
 	
-	def win_round(self, current_stage):
+	def win_round(self):
 		"""Define method that informs player they have won their current round"""
 		pass
 	
 	
-	def palyer_choice(self, die_1, die_2):
+	def player_choice(self):
 		"""Method to allow player to hold 1, roll 1 or roll both die again"""
 		pass
 
 	
-	def evaluate_hold(self, die_1, die_2):
+	def evaluate_hold(self):
 		"""Method to evaluate and ensure that game rules are followed, i.e. invalid dice can not be held"""
 		pass
 
 		
-	def advance_round(current_stage):
+	def advance_round(self):
 		"""Method to advance player to next stage in the game or to the winning stage"""
 		pass
 
@@ -56,65 +74,73 @@ class Angry_Dice:
 
 class Die:
 	"""Define object that describes dice and implements their actions (refer to imported random library for rolling authenticity)"""
+
 	def __init__(self):
 		"""Define Die library that is comprised of the various dice sides."""
 		self.value = 1
-
-
-	def roll_dice(self):
-		"""Method to roll the dice for game play"""
-		pass
-
-
-
-sides = {
-	1: """    
-	-----  
-       |     |
-       |  0  |
-       |     |
-        -----  
+		self.locked = False
+		# ASCII art representations of each side of the dice.
+		self.sides = {
+			1: """    
+ -----
+|     |
+|  0  |
+|     |
+ -----  
 	   """,
 
-	2: """    -----  
-			 |0    |
-			 |     |
-			 |    0|
-			  -----  
+			2: """    
+ -----  
+|0    |
+|     |
+|    0|
+ -----  
 	   """,
 
-	3: """    -----  
-			 | o o |
-			 |  |  |
-			 | ### |
-			  -----  
-			  RARRR!
+			3: """
+ -----  
+| o o |
+|  |  |
+| ### |
+ -----  
+ RARRR!
 	   """,
 
-	4: """    -----  
-			 |0   0|
-		     |     |
-			 |0   0|
-			  -----  
+			4: """    
+ -----  
+|0   0|
+|     |
+|0   0|
+ -----  
 	   """,
 
-	5: """    -----  
-			 |0   0|
-			 |  0  |
-			 |0   0|
-			  -----  
+			5: """    
+ -----  
+|0   0|
+|  0  |
+|0   0|
+ -----  
 	   """,
 
-	6: """     -----  
-			  |0   0|
-			  |0   0|
-			  |0   0|
-			   -----  
+			6: """     
+ -----  
+|0   0|
+|0   0|
+|0   0|
+ -----  
 	   """	
 
 }				    					  
 
+	def roll(self):
+		"""Define method to roll dice"""
+		self.value = randint(1,6)
+		
+	def __str__(self):
+		"""Returns a string representation of the die."""
+		return self.sides[self.value]
+
 
 
 if __name__ == '__main__':
-		main()
+	main()
