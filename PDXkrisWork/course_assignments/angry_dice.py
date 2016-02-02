6cf4481acfed8d95dd38906d65168d0766e398f4
kriskuchinka <kriskuchinka@gmail.com>
2016-02-02 09:49:03 -0800

    More progress on creation of Angry Dice game.

--- PDXkrisWork/course_assignments/angry_dice.py
+++ PDXkrisWork/course_assignments/angry_dice.py
@@ -1,28 +1,47 @@
 """A computer version of the game Angry Dice."""
 
 __author__ = "Kris Kuchinka"
-__conceptPartner__ = "Sarah Fellows"
 
+"""Created flow chart of game and UML diagram that was used to code this program with Sarah Fellows at Code Academy.
+
+   Creation Date: 2016.01.27
+   Date finished: 
+
+"""
+
+# import randint function from random module to implement dice roll 
 from random import randint
 
 
 def main():
-	current_stage = 1
-	angry = Angry_Dice("Jim")
-	angry.roll_dice()
-	angry.check_angry()
-	print(current_stage)
+	# Create a player using the class Angry_Dice and call player Jim
+	player = Angry_Dice("Jim")
+	# Goal is reach stage 4. Set the beginning stage to 1 for start of game.
+	player.current_stage = 1
+
+	while player.current_stage != 4:
+		# Roll the dice, using function created below and player initiated above
+		player.roll_dice()
+		player.player_choice()
+		player.win_round()
+	
+
+	# Check if both the die are "angry"
+	# Tell player what stage they are on
+	print("Starting Stage:", player.current_stage)
+
+	
+
+	print("Stage after winround", player.current_stage + 1)
 
 
-class Angry_Dice:
+class Angry_Dice():
 	"""Defines the variables and methods needed to play the game."""
 	def __init__(self, player_name):
 		# initalize necessary variables for Angry_Dice
 		self.current_stage = 1
-		self.current_goal = []
-		self.player_name = ""
-		# initialize a dictionary for the Die
-#!#!#!#!# This may be wrong #!#!#!#!#
+		self.stage_goal = {1:[1,2], 2:[3, 4], 3:[5,6]}
+		self.player_name = player_name
 		self.die_1 = Die()
 		self.die_2 = Die()
 
@@ -30,10 +49,12 @@ class Angry_Dice:
 	def roll_dice(self):
 		"""Method to roll the dice for game play"""
 		
+		# Check to make sure that die_1 is not locked by player
 		if not self.die_1.locked:
 			# Roll the dice we need to roll
 			self.die_1.roll()
 
+		# Check to make sure that die_2 is not locked by player
 		if not self.die_2.locked:
 			# Roll the dice we need to roll
 			self.die_2.roll()
@@ -53,22 +74,49 @@ class Angry_Dice:
 	
 	def win_round(self):
 		"""Define method that informs player they have won their current round"""
-		pass
+		print("Current stage:", self.current_stage)
+		current_goal = self.stage_goal[self.current_stage]
 	
-	
+		if self.die_1.value in current_goal and self.die_2.value in current_goal:
+			if self.die_1.value != self.die_2.value:
+				print("Goal met")
+				self.current_stage += 1
+
+		print("Stage after check:", self.current_stage)
+
+
 	def player_choice(self):
 		"""Method to allow player to hold 1, roll 1 or roll both die again"""
 		pass
+		# ask the player if they want to lock the die
+		# evaluate their response (yes or no)
+			# if yes, lock chosen die
+		lock_die_1 = input("Would you like to hold Die 1? (Y)es or (N)o)? ")
+		if lock_die_1.upper() == "Y":
+			self.die_1.locked = True
+		else:
+			lock_die_1.upper() == "N"
+			self.die_1.locked = False
 
-	
+		lock_die_2 = input("Would you like to hold Die 2? (Y)es or (N)o? ")
+		if lock_die_2.upper() == "Y":
+			self.die_2.locked = True
+		else: 
+			lock_die_2.upper() == "N"
+			self.die_2.locked = False
+
+#!>!>!?####---> Possibly not necessary, part of holding decision
 	def evaluate_hold(self):
 		"""Method to evaluate and ensure that game rules are followed, i.e. invalid dice can not be held"""
 		pass
 
-		
-	def advance_round(self):
-		"""Method to advance player to next stage in the game or to the winning stage"""
-		pass
+
+# =====>>> Possibly unnecessary		
+	# def advance_round(self):
+	# 	"""Method to advance player to next stage in the game or to the winning stage"""
+	# 	pass
+		# if player is on Stage 1 and die 1 and 2 values are 1 and 2, advance player to Stage 2
+
 
 
 
