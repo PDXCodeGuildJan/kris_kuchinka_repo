8f657d8b8c553da622bfabee8c00f8cc00941d45
kriskuchinka <kriskuchinka@gmail.com>
2016-02-24 12:33:13 -0800

    Most current version of js.

--- PDXkrisWork/course_assignments/pricing/js/pricing.js
+++ PDXkrisWork/course_assignments/pricing/js/pricing.js
@@ -2,20 +2,30 @@
 // Add the click event handler to the "add-item"
 var addItemButton = document.getElementById("add-item");
 addItemButton.onclick = addItem;
-
+// Add the click event handler to the "add-stock"
 var addStockButton = document.getElementById("add-stock");
 addStockButton.onclick = addStock;
+// Add the click even handler to the "remove-stock" button
+var removeStockButton = document.getElementById("remove-stock");
+removeStockButton.onclick = removeStock;
 
 /* Add the item in the text fields to the inventory
  * list, which is in the table body (id="inventory")
  */
 function addItem() {
+	// Set variable "materialName" to value of anything with id 'name'
 	var materialName = document.getElementById("name").value;
+	// Set variable "price" to value of anything with id 'price'
 	var price = document.getElementById("price").value;
+	// Set variable inStock to anything with id 'in-stock' that is checked
 	var inStock = document.getElementById("in-stock").checked;
+	// if the inStock(checked) item is true
 	if (inStock == true) {
+		// set variable below to yes and it will be used as flag for list
 		inStockConfirm = "Yes";
+	// else if something is not checked
 	} else if (inStock == false) {
+		// set variable below to no and it will be used as a flag for list
 		inStockConfirm = "No";
 	}	
 	
@@ -29,44 +39,39 @@ function addItem() {
 //----> Toggles the inStock status on the selected rows inside of inventory
 function addStock() {
 	// CANNOT USE querySelectorAll
-	console.log("You clicked add stock button.");
-	var checked_selection = document.getElementsByClassName("selector");
-	console.log(checked_selection);
-	var inStock = document.getElementById("in-stock").checked;
-	console.log(inStock);
-	console.log(inStockConfirm);
-	// var inStock = document.getElementById("in-stock").checked;
-	// if (checked_selection == true) {
-	// 	inStockConfirm = "Yes";
-	// } else if (inStock == false) {
-	// 	inStockConfirm = "No";
-	// }
-	for (temp = 0; temp<checked_selection.length; temp++) {
-		if (checked_selection[temp].checked == true) {
-			console.log("This is checked.");
-			inStock == true;
-			return inStock;
-			// console.log(checked_selection[temp])
-			// thsi will select alll of th checked elements
-			// find the parents of these elements
-			// find the instock children of these parents 
 
+	var selection = document.getElementsByClassName("selector");
+	// test that selection has a handle on all nodes with class "selector"
+	console.log("This should be all the nodes:", selection);
+	// create a variable named check to hold a list of the checked items
+	checked = [];
+	// loop through list of selections until they have all been examined
+	for (temp = 0; temp<selection.length; temp++) {
+		// inside the loop, if it is checked, set value to true
+		if (selection[temp].checked == true) {
+			// if condition above is satisfied, add current selection to 
+			// checked list
+			checked.push(selection[temp]);
+		} // end of if
+	} // end of for loop
 
-		} else {
-			console.log("This is not checked.");
-		}
-	}
-
-
-	// Find all the selected things
-
-	// Change the inStock value of selected things
-
-
-
-}
+	// print to console the list of checked items
+	console.log(checked);
+} // end of function addStock
 
 function removeStock() {
 	// USE querySelectorAll
+	var selected = document.querySelectorAll("td>input.selector[type=checkbox]:checked");
+	console.log("This is supposed to print checked boxes-->", selected)
 
+	// checked_boxes = []
+
+	// for (temp=0; temp<selected.length; temp++) {
+
+	// 	if (selected[temp].checked == true) {
+	// 		checked_boxes.push(selected[temp]);
+	// 	}
+	// }
+
+	// console.log(checked_boxes)
 }
