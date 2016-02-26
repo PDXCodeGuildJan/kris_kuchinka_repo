1e9f1c06717dcce8fc038e97911e2463a39e93e5
kriskuchinka <kriskuchinka@gmail.com>
2016-02-26 15:54:20 -0800

    Latest updates to files. Things are working properly.

---
+++ PDXkrisWork/course_assignments/javapic/js/join.js
@@ -0,0 +1,21 @@
+/* 
+Created By: Kris Kuchinka
+* Start Date: 2016.02.26
+* Submission Date:
+* Document Purpose: This is an external JavaScript file  
+*   written for the "JavaPic" assignment at PDX Code Guild.
+*/
+
+
+function externalJs() {
+	console.log("Your external JS is connected properly.");
+} // End of start funtion
+
+
+
+
+
+
+
+
+externalJs();
