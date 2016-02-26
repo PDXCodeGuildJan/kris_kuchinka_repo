4ffc48f1d45dea324995f42c76ba407255dc9674
kriskuchinka <kriskuchinka@gmail.com>
2016-02-26 11:16:16 -0800

    Got partially working image changes on click.

--- PDXkrisWork/course_assignments/javapic/js/index.js
+++ PDXkrisWork/course_assignments/javapic/js/index.js
@@ -1,6 +1,24 @@
 function start() {
 	console.log("Your external JS is connected properly.");
-}
+} // end of start funtion
 
-start()
+function changeImage() {
+	var changing_image = document.getElementById("jumbotron");
+	console.log(changing_image);
+
+	changing_image.style.backgroundImage = "url('images/pdxcg_" + image_number + ".jpg')";
+	
+	if (image_number <= 9) {
+		changing_image = "url('images/pdxcg_0" + image_number + ".jpg')";
+	} else {
+		changing_image = "url('images/pdxcg_" + image_number + ".jpg'";
+	};
+	
+	image_number += 1;
+
+} // end of changeImage function
+start();
+// create global variable for image_number 
+var image_number = 1;
+window.onclick = changeImage;
 
