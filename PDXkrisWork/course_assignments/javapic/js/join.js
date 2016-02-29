796a3c659f282ebff6d7b041575e59ac78ebb150
kriskuchinka <kriskuchinka@gmail.com>
2016-02-29 10:52:30 -0800

    New validation functions in process.

--- PDXkrisWork/course_assignments/javapic/js/join.js
+++ PDXkrisWork/course_assignments/javapic/js/join.js
@@ -19,7 +19,46 @@ function disableBrowserValidation() {
 	document.getElementById("signup").noValidate = true;
 }
 
-function
+// create a function to keep form from submitting
+function stopSubmit() {
+	// Select the form by ID and add "listener" for the submit event
+	// In the addEventListener function, feed it the variable submit to be listened for and an anonymous function
+	document.getElementById("signup").addEventListener("submit", function(event) {
+		// Pass the event from anonymous function and attach it to preventDefault function
+		event.preventDefault()
+		// Verify in the console that the submission didn't work
+		console.log("Submission suppressed.");
+	});
+}
+
+function checkName() {
+	var name = document.forms["signup"]["name"].value;
+	if (name == null || name == "") {
+		stopSubmit();
+		alert("You forgot your name!");
+		return false;
+	};
+}
+
+function checkUserName() {
+	var user_name = document.forms["signup"]["username"].value;
+	if (user_name == null || user_name == "") {
+		alert("You didn't enter a username...");
+		stopSubmit();
+		return false;
+	}
+}
+
+function checkEmail() {
+	var email = document.forms["signup"]["email"].value;
+	if (email == null || email == "") {
+		alert("You didn't enter an email address");
+		stopSubmit();
+		return false;
+	} else if (email != null && email =="") {
+		alert("Hell yeah, you entered an email address!");
+	}
+}
 
 
 
@@ -29,3 +68,6 @@ function
 
 externalJs();
 disableBrowserValidation();
+checkName();
+checkUserName();
+checkEmail()
