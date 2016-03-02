f9dd409178e429b00ba8722eb5ee0eeb619937b3
kriskuchinka <kriskuchinka@gmail.com>
2016-03-02 11:47:34 -0800

    Further dev and changes on files.

--- PDXkrisWork/course_assignments/javapic/js/gallery.js
+++ PDXkrisWork/course_assignments/javapic/js/gallery.js
@@ -3,23 +3,34 @@ function externalJs() {
 } // End of start funtion
 
 function displayImages() {
-	var list_item = document.createElement("LI");
-	var picture = document.createElement("IMG");
-	picture.src = "images/pdxcg_01.jpg";
-	for (var pic = 0; pic < picture.length; i++) {
-		if (picture.length < 10) {
-			picture.src = "images/pdxcg_0" + pic + ".jpg";
-			pic++;	
-		} else if (picture.length >= 10) {
-			picture.src = "images/pdxcg_" + pic + ".jpg";
-			pic++;
+	// Grab gallery section
+	var gallery = document.getElementById("gallery");
+	// Create unordered list element
+	var unorderedElement = document.createElement("UL");
+	// loop through pic numbers to add to image elements
+	for (var pic = 1; pic <= 60; pic++) {
+		// Create list item element
+		var listElement = document.createElement("LI");
+		// Create img item element
+		var imageElement = document.createElement("IMG");
+		if (pic < 10) {
+			// Give the image taga a source and insert current pic number to loop through
+			imageElement.src = "images/pdxcg_0" + pic + ".jpg";
+		} else if (pic == 42) {
+			continue;
+		} else if (pic >= 10) {
+			// Give the image taga a source and insert current pic number to loop through		
+			imageElement.src = "images/pdxcg_" + pic + ".jpg";
 		};
-
+		// make the imageElement a child of the listItem
+		listElement.appendChild(imageElement);
+		// Make the listElement a child of the unorderedElement
+		unorderedElement.appendChild(listElement);
 	};
-	list_item.appendChild(picture);
-	document.getElementById("gallery").appendChild(list_item);
-	console.log("Something.");
-}
+	// Put the whole unordered list that has been created // into the gallery
+	gallery.appendChild(unorderedElement);
+} // end of displayImages function
+
 
 externalJs();
 
