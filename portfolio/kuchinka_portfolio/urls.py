d71dbceb29908ff615c887d4ae6ad89fca3cb73c
kriskuchinka <kriskuchinka@gmail.com>
2016-03-15 16:26:46 -0700

    Got landing page working via views and both URL patterns. Next step is to add a project on as an app.

--- portfolio/kuchinka_portfolio/urls.py
+++ portfolio/kuchinka_portfolio/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from display_port.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #--!--> I'm a little unsure, but I think this is how you register a home url
-    url(r'^$', index.site.urls),
+    url(r'^$', index),
 ]
